3142a58d2933bb792b77b6476c40b0dd16b2797e
Olivia Ruiz-Knott <livvy@hovercraftstudio.com>
2025-11-24 12:12:44 -0700

    add color palettes as a separate module and import into main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as PIXI from "pixi.js";
 import vertex from "./shaders/vertex.glsl?raw";
 import fragment from "./shaders/fragment.glsl?raw";
+import { colorPalettes } from "./colorPalettes.js";
 
 // Get the canvas container
 const container = document.getElementById("canvas-container");
@@ -17,35 +18,6 @@ await app.init({
 
 // Add canvas to the container
 container.appendChild(app.canvas);
-
-// Color palettes for different modes
-const colorPalettes = {
-  workspace: {
-    color1: [0.2, 0.4, 0.8],
-    color2: [0.4, 0.6, 0.9],
-    color3: [0.3, 0.8, 0.7],
-  },
-  chill: {
-    color1: [0.6, 0.4, 0.8],
-    color2: [0.8, 0.5, 0.7],
-    color3: [0.5, 0.6, 0.9],
-  },
-  photo: {
-    color1: [1.0, 0.6, 0.3],
-    color2: [1.0, 0.8, 0.3],
-    color3: [0.9, 0.4, 0.5],
-  },
-  date: {
-    color1: [0.9, 0.2, 0.4],
-    color2: [0.8, 0.3, 0.6],
-    color3: [0.6, 0.2, 0.5],
-  },
-  nightclub: {
-    color1: [0.8, 0.0, 0.8],
-    color2: [0.0, 0.8, 0.8],
-    color3: [0.8, 0.8, 0.0],
-  },
-};
 
 // Create filter using PIXI.Filter with GlProgram
 const gradientFilter = new PIXI.Filter({
